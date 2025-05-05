564294e8042b4cdf313a60ddb673cc05cebdf0e4
Yilin Fang <qzfyl98@outlook.com>
2025-05-05 17:57:50 -0400

    Swith to python built-in logging

--- backup-bw.py
+++ backup-bw.py
@@ -2,6 +2,7 @@
 
 import argparse
 import json
+import logging
 import os
 import shutil
 import subprocess
@@ -33,15 +34,12 @@ with open(CONFIG_FILE, "r") as f:
 # Ensure log directory exists
 os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
 
-
-# Function to log messages
-def log(message):
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    log_message = f"{timestamp} - {message}"
-    print(log_message)
-    with open(LOG_FILE, "a") as log_file:
-        log_file.write(log_message + "\n")
-
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler(LOG_FILE), logging.StreamHandler()],
+)
 
 # Ensure the backup directory exists
 os.makedirs(BACKUP_DIR, exist_ok=True)
@@ -54,33 +52,30 @@ def is_command_available(command):
 
 # Ensure Bitwarden CLI is installed
 if not is_command_available("bw"):
-    log(
-        "ERROR: Bitwarden CLI (bw) could not be found in default PATH. Please install it."
+    logging.error(
+        "Bitwarden CLI (bw) could not be found in default PATH. Please install it."
     )
-    # show default path in log
-    log(f"INFO: Default PATH: {os.environ['PATH']}")
-    # export /usr/local/bin/ to PATH
+    logging.info(f"Default PATH: {os.environ['PATH']}")
     os.environ["PATH"] += os.pathsep + "/usr/local/bin"
-    # export /opt/homebrew/bin/ to PATH
     os.environ["PATH"] += os.pathsep + "/opt/homebrew/bin"
-    log(f"INFO: Updated PATH: {os.environ['PATH']}")
+    logging.info(f"Updated PATH: {os.environ['PATH']}")
     if not is_command_available("bw"):
-        log("ERROR: Bitwarden CLI (bw) is still not available.")
+        logging.error("Bitwarden CLI (bw) is still not available.")
         exit(1)
 else:
-    log("INFO: Bitwarden CLI (bw) is available.")
+    logging.info("Bitwarden CLI (bw) is available.")
 
 # Log in to Bitwarden
-log("INFO: Logging into Bitwarden...")
+logging.info("Logging into Bitwarden...")
 login_env = os.environ.copy()
 login_env["BW_CLIENTID"] = BW_CLIENTID
 login_env["BW_CLIENTSECRET"] = BW_CLIENTSECRET
 if subprocess.run(["bw", "login", "--apikey"], env=login_env).returncode != 0:
-    log("ERROR: Failed to login to Bitwarden")
+    logging.error("Failed to login to Bitwarden")
     exit(1)
 
 # Unlock the vault
-log("INFO: Unlocking the vault...")
+logging.info("Unlocking the vault...")
 unlock_env = os.environ.copy()
 unlock_env["BW_PASSWORD"] = BW_PASSWORD
 result = subprocess.run(
@@ -90,22 +85,22 @@ result = subprocess.run(
     env=unlock_env,
 )
 if result.returncode != 0:
-    log("ERROR: Failed to unlock the vault")
+    logging.error("Failed to unlock the vault")
     exit(1)
 session = result.stdout.strip()
-log("INFO: Vault unlocked successfully.")
+logging.info("Vault unlocked successfully.")
 
 # Sync the vault
-log("INFO: Syncing the vault...")
+logging.info("Syncing the vault...")
 if subprocess.run(["bw", "sync", "--session", session]).returncode != 0:
-    log("ERROR: Failed to sync the vault")
+    logging.error("Failed to sync the vault")
     exit(1)
 
 # Export the vault
 timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 backup_file = os.path.join(SCRIPT_DIR, f"bitwarden_backup_{timestamp}.json")
 encrypted_backup_file = os.path.join(BACKUP_DIR, f"bitwarden_backup_{timestamp}.zip")
-log(f"INFO: Exporting vault to {backup_file}...")
+logging.info(f"Exporting vault to {backup_file}...")
 if (
     subprocess.run(
         [
@@ -121,30 +116,30 @@ if (
     ).returncode
     != 0
 ):
-    log("ERROR: Failed to export the vault")
+    logging.error("Failed to export the vault")
     exit(1)
 
 # Encrypt the backup
-log(f"INFO: Encrypting backup to {encrypted_backup_file}...")
+logging.info(f"Encrypting backup to {encrypted_backup_file}...")
 if (
     subprocess.run(
         ["zip", "-P", ZIP_PASSWORD, "-j", encrypted_backup_file, backup_file]
     ).returncode
     != 0
 ):
-    log("ERROR: Failed to encrypt the backup")
+    logging.error("Failed to encrypt the backup")
     exit(1)
 os.remove(backup_file)
-log("INFO: Plain backup file removed after encryption.")
+logging.info("Plain backup file removed after encryption.")
 
 # Log out from Bitwarden
-log("INFO: Logging out from Bitwarden...")
+logging.info("Logging out from Bitwarden...")
 if subprocess.run(["bw", "logout"]).returncode != 0:
-    log("ERROR: Failed to log out from Bitwarden")
+    logging.error("Failed to log out from Bitwarden")
     exit(1)
 
 # Cleanup old backups
-log("INFO: Cleaning up old backups...")
+logging.info("Cleaning up old backups...")
 backup_files = sorted(
     [
         f
@@ -156,8 +151,8 @@ backup_files = sorted(
 if len(backup_files) > MAX_BACKUPS:
     for old_backup in backup_files[: len(backup_files) - MAX_BACKUPS]:
         os.remove(os.path.join(BACKUP_DIR, old_backup))
-        log(f"INFO: Deleted old backup: {old_backup}")
+        logging.info(f"Deleted old backup: {old_backup}")
 else:
-    log("INFO: No old backups to delete.")
+    logging.info("No old backups to delete.")
 
-log("INFO: Backup completed successfully.")
+logging.info("Backup completed successfully.")
